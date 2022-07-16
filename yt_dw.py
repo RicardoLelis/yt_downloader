648b81037e71a4c4ea02742ef53b8d42a5140453
Ricardo Lélis <Ricardo Lelis>
2022-07-16 22:57:33 +0100

    Added single link download feature

--- yt_dw.py
+++ yt_dw.py
@@ -4,5 +4,13 @@ from sys import argv
 link = argv[1]
 yt = YouTube(link)
 
+# Video Details
 print(f"Title: {yt.title}")
+
 print(f"Views: {yt.views}")
+
+# Video Download
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download("/Users/lelisra/Downloads/")
