e2b4dbcd391b4e59d3710022f98569df8fc5017f
Ricardo Lélis <Ricardo Lelis>
2022-07-21 17:20:17 +0100

    Added playlist feature. Created functions for single and playlist dw

--- yt_dw.py
+++ yt_dw.py
@@ -1,8 +1,19 @@
 from pytube import YouTube
+from pytube import Playlist
 from sys import argv
 
+# DOCS: https://pytube.io/en/latest/user/quickstart.html
+
 link = argv[1]
+playlist_link = argv[2]
 yt = YouTube(link)
+
+# Playlists
+# Name: Yoga For Men
+playlist_link = "https://youtube.com/playlist?list=PLBfRLLhSBb-B8-1v35ueWDRaZtyr_boHN"
+p = Playlist(playlist_link)
+for video in p.videos:
+    print(video.title, " Views: ", video.views)
 
 # Video Details
 print(f"Title: {yt.title}")
@@ -13,4 +24,13 @@ print(f"Views: {yt.views}")
 
 yd = yt.streams.get_highest_resolution()
 
-yd.download("/Users/lelisra/Downloads/")
+# Functions
+def download_video(url, destination_folder):
+    yt = YouTube(url)
+    yd = yt.streams.get_highest_resolution()
+    yd.download(destination_folder)
+
+def download_playlist(url):
+    p = Playlist(url)
+    for video in p.videos:
+        video.streams.first().download()
